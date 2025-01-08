2878d3998ab71126067ee38d6a3130164a550937
jainoos <ammjainoos@gmail.com>
2025-01-08 23:20:35 +0530

    create hero comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./Components/NabBar/Hero/Hero";
 import NavBar from "./Components/NabBar/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 }
