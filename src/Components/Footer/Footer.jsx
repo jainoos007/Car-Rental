04ab90fb078ac996000f92194ede103594b9b7c2
jainoos <ammjainoos@gmail.com>
2025-01-16 02:20:53 +0530

    finish footer comp

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,83 +10,126 @@ import {
 
 const Footer = () => {
   return (
-    <div className="container grid grid-cols-4 mt-20">
-      <div className="space-y-6">
-        <h1 className="font-bold text-4xl">Car Rental</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sunt a
-          vitae numquam aliquam provident.
-        </p>
-        <div className="flex">
-          <FaLocationArrow />
-          <p>Mumbai, Maharashtra</p>
+    <div className="bg-gray-100 rounded-t-3xl dark:bg-gray-900 dark:text-white">
+      <div className="container grid grid-cols-10  mt-20 py-16">
+        <div className="col-span-4 space-y-6 mr-10">
+          <h1 className="font-bold text-4xl">Car Rental</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sunt a
+            vitae numquam aliquam provident.
+          </p>
+          <div className="flex gap-3">
+            <div className="flex justify-center items-center">
+              <FaLocationArrow />
+            </div>
+            <p>Mumbai, Maharashtra</p>
+          </div>
+          <div className="flex gap-3">
+            <div className="flex justify-center items-center">
+              <FaMobileAlt />
+            </div>
+            <p>+91 123456789</p>
+          </div>
+          <div className="flex text-3xl gap-3">
+            <FaInstagram className="hover:text-primary duration-300 cursor-pointer" />
+            <FaFacebook className="hover:text-primary duration-300 cursor-pointer" />
+            <FaLinkedin className="hover:text-primary duration-300 cursor-pointer" />
+          </div>
         </div>
-        <div className="flex">
-          <FaMobileAlt />
-          <p>+91 123456789</p>
+        {/* Links */}
+        <div className="col-span-2 flex flex-col gap-3">
+          <h1 className="font-bold text-xl">Important Links</h1>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            Home
+          </a>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            About
+          </a>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            Contacts
+          </a>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            Blog
+          </a>
         </div>
-        <div className="flex">
-          <FaInstagram />
-          <FaFacebook />
-          <FaLinkedin />
+        <div className="col-span-2 flex flex-col gap-3">
+          <h1 className="font-bold text-xl">Important Links</h1>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            Home
+          </a>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            About
+          </a>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            Contacts
+          </a>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            Blog
+          </a>
         </div>
-      </div>
-      <div>
-        <h1>Important Links</h1>
-        <a href="/#Home">
-          <FaChevronRight />
-          Home
-        </a>
-        <a href="/#Home">
-          <FaChevronRight />
-          About
-        </a>
-        <a href="/#Home">
-          <FaChevronRight />
-          Contacts
-        </a>
-        <a href="/#Home">
-          <FaChevronRight />
-          Blog
-        </a>
-      </div>
-      <div>
-        <h1>Important Links</h1>
-        <a href="/#Home">
-          <FaChevronRight />
-          Home
-        </a>
-        <a href="/#Home">
-          <FaChevronRight />
-          About
-        </a>
-        <a href="/#Home">
-          <FaChevronRight />
-          Contacts
-        </a>
-        <a href="/#Home">
-          <FaChevronRight />
-          Blog
-        </a>
-      </div>
-      <div>
-        <h1>Important Links</h1>
-        <a href="/#Home">
-          <FaChevronRight />
-          Home
-        </a>
-        <a href="/#Home">
-          <FaChevronRight />
-          About
-        </a>
-        <a href="/#Home">
-          <FaChevronRight />
-          Contacts
-        </a>
-        <a href="/#Home">
-          <FaChevronRight />
-          Blog
-        </a>
+        <div className="col-span-2 flex flex-col gap-3">
+          <h1 className="font-bold text-xl">Important Links</h1>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            Home
+          </a>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            About
+          </a>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            Contacts
+          </a>
+          <a
+            href="/#Home"
+            className="flex items-center gap-1 hover:text-primary duration-300"
+          >
+            <FaChevronRight />
+            Blog
+          </a>
+        </div>
       </div>
     </div>
   );
