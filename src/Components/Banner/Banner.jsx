37c5f6994c5163475250ebdc55287ce6f79f246e
jainoos <ammjainoos@gmail.com>
2025-01-18 00:46:21 +0530

    adding aos to banner comp

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -17,14 +17,29 @@ const Banner = () => {
           className="rounded-xl flex flex-col justify-center items-center gap-5"
           style={styleImg}
         >
-          <h1 className="font-serif font-bold text-4xl">
+          <h1
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="100"
+            className="font-serif font-bold text-4xl"
+          >
             Get Started with our app
           </h1>
-          <p className="text-center px-40">
+          <p
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="200"
+            className="text-center px-40"
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, iure
             enim dolorum fugit esse reiciendis!
           </p>
-          <div className="flex gap-4">
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="300"
+            className="flex gap-4"
+          >
             <img
               src={img2}
               alt=""
