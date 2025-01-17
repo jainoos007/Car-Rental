8cc491e4847d84304909420a037ec2c2e019e04e
jainoos <ammjainoos@gmail.com>
2025-01-18 00:40:04 +0530

    adding aos to Testimonial comp

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -5,18 +5,21 @@ const cartItems = [
   {
     id: 1,
     name: "Rohit",
+    delay: "100",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
     img: "https://picsum.photos/101/101",
   },
   {
     id: 2,
     name: "Virat",
+    delay: "300",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
     img: "https://picsum.photos/201/201",
   },
   {
     id: 3,
     name: "Sam",
+    delay: "500",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
     img: "https://picsum.photos/301/301",
   },
@@ -26,14 +29,23 @@ const Testimonial = () => {
   return (
     <div className="dark:bg-black dark:text-white">
       <div className="container flex flex-col items-center space-y-5 py-20">
-        <h1 className="font-serif font-bold text-4xl">
+        <h1
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          className="font-serif font-bold text-4xl"
+        >
           What Our Clients Say About Us
         </h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        <p data-aos="fade-up" data-aos-duration="1000">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </p>
         {/* Testimonial Cards */}
         <div className="grid grid-cols-3 gap-7">
           {cartItems.map((item) => (
             <div
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay={item.delay}
               key={item.id}
               className="bg-gray-200 dark:bg-gray-900 px-5 py-10 flex flex-col gap-10 text-center rounded-md mt-7"
             >
