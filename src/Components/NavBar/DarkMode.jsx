5cf3fab1008401be2f18f95cb6613e4188900537
jainoos <ammjainoos@gmail.com>
2025-01-19 05:59:11 +0530

    make responsive navbar

--- src/Components/NavBar/DarkMode.jsx
+++ src/Components/NavBar/DarkMode.jsx
@@ -4,13 +4,13 @@ const DarkMode = ({ theme, setTheme }) => {
   return (
     <div className="relative">
       <MdLightMode
-        className={`text-2xl cursor-pointer transition-all duration-300 absolute top-1 z-10 ${
+        className={`text-3xl cursor-pointer transition-all duration-300 absolute top-0 z-10 ${
           theme === "dark" ? "opcaity-100" : "opacity-0"
         }`}
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       />
       <MdDarkMode
-        className={`text-2xl cursor-pointer transition-all duration-300 absolute top-1 z-10 ${
+        className={`text-3xl cursor-pointer transition-all duration-300 absolute top-0 z-10 ${
           theme === "light" ? "opcaity-100" : "opacity-0"
         }`}
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,32 +4,32 @@ import DarkMode from "./DarkMode";
 const NavBar = ({ theme, setTheme }) => {
   return (
     <div className="shadow-md dark:bg-gray-900 dark:text-white">
-      <div className="container w-full flex justify-between items-center py-3 dark:">
+      <div className="container w-full flex justify-between items-center py-3">
         <div>
-          <p className=" font-serif font-bold text-3xl">Car Rental</p>
+          <p className=" font-serif font-bold text-4xl">Car Rental</p>
         </div>
-        <div className="gap-5 flex">
+        <div className="gap-5  hidden md:block md:flex">
           <a
             href="#/Home"
-            className="font-semibold text-lg hover:text-primary hover:underline-offset-8 hover:underline transition-all duration-400"
+            className="font-semibold text-xl hover:text-primary hover:underline-offset-8 hover:underline transition-all duration-400"
           >
             HOME
           </a>
           <a
             href="#/Home"
-            className="font-semibold text-lg hover:text-primary hover:underline-offset-8 hover:underline transition-all duration-400"
+            className="font-semibold text-xl hover:text-primary hover:underline-offset-8 hover:underline transition-all duration-400"
           >
             CARS
           </a>
           <a
             href="#/Home"
-            className="font-semibold text-lg hover:text-primary hover:underline-offset-8 hover:underline transition-all duration-400"
+            className="font-semibold text-xl hover:text-primary hover:underline-offset-8 hover:underline transition-all duration-400"
           >
             ABOUT
           </a>
           <a
             href="#/Home"
-            className="font-semibold text-lg hover:text-primary hover:underline-offset-8 hover:underline transition-all duration-400"
+            className="font-semibold text-xl hover:text-primary hover:underline-offset-8 hover:underline transition-all duration-400"
           >
             BOOKING
           </a>
